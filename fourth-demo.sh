42d8571dc1beb5f2e7b5360b0e88183ab6fb91a2
rohitutekar123 <90674086+rohitutekar123@users.noreply.github.com>
2024-12-31 19:12:00 +0530

    Add files via upload

---
+++ fourth-demo.sh
@@ -0,0 +1,11 @@
+#assigning value to the variable 
+a=10
+b=5
+
+#comparing value and print the greater number
+
+if [ $a > $b ]
+then echo "a is greater than b"
+else 
+	echo "b is greater than a"
+fi
