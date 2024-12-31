15392e0854e77a34a4210779c260de21c6c63e3d
rohitutekar123 <90674086+rohitutekar123@users.noreply.github.com>
2024-12-31 18:12:32 +0530

    Update third-demo.sh

--- third-demo.sh
+++ third-demo.sh
@@ -6,6 +6,8 @@
 
 ## No need to use echo to print the print statements of isk memory and CPU debug mode automatically print it ##
 set -x # Debug mode
+set -e #it exit the script if error will generate
+set -o #it exit the script if error will generate if "|" pipe is present
 
 #echo " Print Disk Space "
 df -h
